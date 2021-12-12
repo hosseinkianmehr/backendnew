8d28c1bdadf502c7ffa454b673124e19a00af179
unknown <hosseinkianmehr@yahoo.com>
2021-12-12 22:18:00 +0330

    create admin_django

--- backendSolar/TagAndComment/admin.py
+++ backendSolar/TagAndComment/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import tag 
+admin.site.register(tag)
--- backendSolar/TagAndComment/models.py
+++ backendSolar/TagAndComment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class tag(models.Model):
+    titel = models.CharField('titel', max_length=50,unique=True )
+    
+
+    def __str__(self):
+        return self.titel
+    class Meta:
+        verbose_name = ("tag")
+        verbose_name_plural = ("tags")
+   
